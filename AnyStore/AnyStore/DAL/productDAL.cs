fd89a928b219510f8559e76c925c55d545e8045e
Dinod Deshanjana <dinoddeshanjana20@gmail.com>
2025-12-10 20:16:32 +0530

    Add productDAL for product CRUD operations
    Introduced productDAL class to handle product data access including select, insert, update, and delete methods. Updated AnyStore.csproj to include the new DAL file.

---
+++ AnyStore/AnyStore/DAL/productDAL.cs
@@ -0,0 +1,183 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class productDAL
+    {
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+
+        #region Select Method for Product Module
+        public DataTable Select()
+        {
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                string sql = "SELECT * FROM tbl_products";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+
+                conn.Open();
+
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        #endregion
+        #region Method to Insert Product in Database
+        public bool Insert(productsBLL p)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                String sql = "INSERT INTO tbl_products (name, category, discription, rate, qty, added_date, added_by) VALUES    (@name, @category, @discription, @rate, @qty, @added_date, @added_by)";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@discription", p.discription);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region Mehtod to Update Product in Database
+        public bool Update(productsBLL p)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                string sql = "UPDATE tbl_products  SET name=@name, category=@category, discription=@discription, rate=@rate, qty=@qty, added_date=@added_date, added_by=@added_by WHERE id=@id";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@name", p.name);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@discription", p.discription);
+                cmd.Parameters.AddWithValue("@rate", p.rate);
+                cmd.Parameters.AddWithValue("@qty", p.qty);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@added_by", p.added_by);
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+            return isSuccess;
+        }
+        #endregion
+        #region Method to Delete product in Database
+        public bool Delete(productsBLL p)
+        {
+            bool isSuccess = false;
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                string sql = "DELETE FROM tbl_products WHERE id=@id";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+    }
+}
